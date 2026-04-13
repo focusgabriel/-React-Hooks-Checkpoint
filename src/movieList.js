28194f16d7beef9aecca94f1c331df9526a99436
focusgabriel <focusgabriel@users.noreply.github.com>
2026-04-13 11:14:20 +0100

    changed the description of the movies list

--- src/movieList.js
+++ src/movieList.js
@@ -2,19 +2,19 @@
   const Movies = [
     {
       title: "Game of Thrones",
-      description: "Drama, Action, Adventure",
+      description: "Game of Thrones is a drama based movie full of adventures, fairytale and a war of succession among competing claimants for control of the Iron Throne of the Seven Kingdoms, with other noble families fighting for independence from the throne",
       rating: 9.0,
       posterURL: "https://gameofthrones.com"
     },
     {
       title: "Viking",
-      description: "Drama, Action, Adventure",
+      description: "Scandinavian seafaring people—raiders, traders, explorers, and settlers—from present-day Norway, Denmark, and Sweden, who dominated Northern Europe and beyond from the late 8th to the mid-11th centuries",
       rating: 8.5,
       posterURL: "https://vikings.com"
     },
     {
       title: "A Day of the Jackal",
-      description: "Action, Thriller, Crime",
+      description: 'The Assassin ("The Jackal"): Portrayed as a master of disguise, methodical, and operating alone with no known identity. He is incredibly precise, often using custom weapons',
       rating: 8.7,
       posterURL: "https://dayofthejackal.com"
     }
